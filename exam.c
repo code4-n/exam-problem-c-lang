c1f55fc698dcccb12b206b67d941e3cf4e7737a1
igor <igor_batinic@yahoo.com>
2019-10-29 11:55:30 +0100

    Basic structure of c program and push test.

---
+++ exam.c
@@ -0,0 +1,15 @@
+// igor batinic
+// 29.10.2019.
+// exam problem
+
+#include <stdio.h>
+
+int main(void)
+
+{
+    for ( int i = 0; i < 5; i++)
+    {
+    printf("Hello, world!\n");
+    }
+    return 0;
+}
